3549cdcd52d415b8df94907f7ae76c4bc31f215d
Zhiqiang Liu <aaron@magicwindow.cn>
2019-10-16 11:37:41 +0800

    播放声音优化

--- libraryframework/src/main/java/com/theone/framework/util/BeeAndVibrateUtils.kt
+++ libraryframework/src/main/java/com/theone/framework/util/BeeAndVibrateUtils.kt
@@ -26,19 +26,34 @@ import java.io.IOException
  * @Date 2019-06-15
  * @Description 管理类(振动:[Vibrator]+提示音:[SoundPool],[MediaPlayer],[Ringtone])
  */
-object BeeAndVibrateUtils {
-    private const val TAG = "BeeAndVibrateUtils"
+class BeeAndVibrateUtils private constructor(context: Context) {
 
-    private const val BEEP_VOLUME = 0.1F
-    private const val VIBRATE_DURATION = 100L
-    private const val VIBRATE_WAVE_DURATION = 130L
+    /**
+     * 声音播放，轻量，适合播放较短提示音。
+     * !!!必须在合适时机调用soundPool.release()释放资源!!!
+     */
+    var soundPool: SoundPool? = null
 
-    private fun shouldPlayBeep(context: Context): Boolean?  {
-        val audioService = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        //如果手机不是普通模式就不让其有声音
-        return audioService.ringerMode == AudioManager.RINGER_MODE_NORMAL
+    /**
+     * 播放声音，较重，不限播放时长
+     */
+    var mediaPlayer: MediaPlayer? = null
+
+    fun getInstance(context: Context): BeeAndVibrateUtils {
+        if (instance == null) {
+            synchronized(BeeAndVibrateUtils::class) {
+                if (instance == null) {
+                    instance = BeeAndVibrateUtils(context)
+                }
+            }
+        }
+        return instance!!
     }
 
+
+    init {
+        initSoundPool(context)
+    }
 
     /**
      * 一次振动
@@ -49,7 +64,12 @@ object BeeAndVibrateUtils {
     fun vibrate(context: Context?, durationMill: Long = VIBRATE_DURATION) {
         val vibrator = context?.getSystemService(Service.VIBRATOR_SERVICE) as Vibrator?
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            vibrator?.vibrate(VibrationEffect.createOneShot(durationMill, VibrationEffect.DEFAULT_AMPLITUDE))
+            vibrator?.vibrate(
+                VibrationEffect.createOneShot(
+                    durationMill,
+                    VibrationEffect.DEFAULT_AMPLITUDE
+                )
+            )
         } else {
             vibrator?.vibrate(durationMill)
         }
@@ -83,32 +103,19 @@ object BeeAndVibrateUtils {
      * 播放成功声音
      * @param context Context
      */
-    fun playBeeSuccess(context: Context?): SoundPool? {
-        return playSound(
-            context,
-            R.raw.beep
-        )
+    fun playBeeSuccess(): SoundPool? {
+        return playSound(SOUND_SUCCESS)
     }
 
     /**
      * 播放错误声音
      * @param context Context
      */
-    fun playBeeError(context: Context?): SoundPool? {
-        return playSound(
-            context,
-            R.raw.beep
-        )
+    fun playBeeError(): SoundPool? {
+        return playSound(SOUND_ERROR)
     }
 
-
-    /**
-     * 声音播放，轻量，适合播放较短提示音。
-     * !!!必须在合适时机调用soundPool.release()释放资源!!!
-     */
-    var soundPool: SoundPool? = null
-
-    private fun playSound(context: Context?, @RawRes rawId: Int): SoundPool? {
+    private fun initSoundPool(context: Context?): SoundPool? {
         if (context == null) return null
         if (soundPool == null) {
             soundPool = if (Build.VERSION.SDK_INT >= 21) {
@@ -121,17 +128,25 @@ object BeeAndVibrateUtils {
             } else {
                 SoundPool(1, AudioManager.STREAM_SYSTEM, 5)
             }
-
-            soundPool?.setOnLoadCompleteListener { pool, _, _ ->
-                pool.play(1,
-                    BEEP_VOLUME,
-                    BEEP_VOLUME, 0, 0, 1F)
-            }
-
         }
-        soundPool?.load(context, rawId, 1)
+        //加载“成功”音效
+        SOUND_SUCCESS = soundPool?.load(context, R.raw.beep, 1) ?: 1
+        //加载“失败”音效
+        SOUND_ERROR = soundPool?.load(context, R.raw.beep, 1) ?: 2
+        return soundPool
+    }
 
-
+    /**
+     * 声音播放，轻量，适合播放较短提示音。
+     *  左\右声道的音量控制, 0.0 到 1.0
+     * @param number Int 音频的序号(load到SoundPool的顺序，从1开始)
+     */
+    private fun playSound(number: Int): SoundPool? {
+        soundPool?.play(
+            number,
+            BEEP_VOLUME,
+            BEEP_VOLUME, 0, 0, 1F
+        )
         return soundPool
     }
 
@@ -151,13 +166,20 @@ object BeeAndVibrateUtils {
 
     /**
      * 播放声音，较重，不限播放时长
+     *
      * @param context Context
      * @param listener PlayerCompleteListener 允许为空
      */
-    var mediaPlayer: MediaPlayer? = null
-
-    fun playMediaSound(context: Context?, @RawRes id: Int, listener: PlayerCompleteListener? = null): MediaPlayer? {
-        if (mediaPlayer == null) mediaPlayer = MediaPlayer() else mediaPlayer?.reset()
+    fun playMediaSound(
+        context: Context?, @RawRes id: Int,
+        listener: PlayerCompleteListener? = null
+    ): MediaPlayer? {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer()
+        } else {
+            mediaPlayer?.release()
+            mediaPlayer?.reset()
+        }
         if (Build.VERSION.SDK_INT >= 21) {
             val attributes = AudioAttributes.Builder()
                 .setUsage(AudioAttributes.USAGE_ALARM)
@@ -187,8 +209,6 @@ object BeeAndVibrateUtils {
                 }
                 mediaPlayer?.prepareAsync()
             }
-
-
         } catch (ioe: IOException) {
             Log.e(TAG, "$ioe")
             mediaPlayer?.release()
@@ -197,11 +217,31 @@ object BeeAndVibrateUtils {
         return mediaPlayer
     }
 
+    /**
+     * 释放播放资源
+     */
+    fun release() {
+        mediaPlayer?.release()
+        soundPool?.release()
+    }
+
 
     //MediaPlayer播放完毕监听
     interface PlayerCompleteListener {
         fun onComplete(mp: MediaPlayer)
     }
 
+    companion object {
+        private const val TAG = "BeeAndVibrateUtils"
+
+        private const val BEEP_VOLUME = 0.1F;
+        private const val VIBRATE_DURATION = 100L;
+        private const val VIBRATE_WAVE_DURATION = 130L;
+
+        private var SOUND_SUCCESS = 1;
+        private var SOUND_ERROR = 2;
+
+        private var instance: BeeAndVibrateUtils? = null
+    }
 
 }
