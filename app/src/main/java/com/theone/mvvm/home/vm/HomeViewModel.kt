1d993c5bf2f7f9c67608896159beecca82db6048
Zhiqiang Liu <aaron@magicwindow.cn>
2020-10-16 19:51:03 +0800

    架构util升级

--- app/src/main/java/com/theone/mvvm/home/vm/HomeViewModel.kt
+++ app/src/main/java/com/theone/mvvm/home/vm/HomeViewModel.kt
@@ -9,5 +9,5 @@ import com.theone.mvvm.home.model.HomeModel
  * @Email liuzhiqiang@moretickets.com
  * @Description
  */
-class HomeViewModel(override var model: HomeModel? = HomeModel()) : BaseViewModel<HomeModel>() {
+class HomeViewModel(override var model: HomeModel = HomeModel()) : BaseViewModel<HomeModel>() {
 }
